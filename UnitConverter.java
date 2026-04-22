15721e10a828be7f372b17686590a343b71440ea
md talib <mdtalib72177@gmail.com>
2026-04-22 07:49:09 +0530

    eat: add volume converter

---
+++ UnitConverter.java
@@ -0,0 +1,176 @@
+import java.util.Scanner;
+
+public class UnitConverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean isTrue = true;
+
+        // Objects
+        Length length = new Length();
+        Weight weight = new Weight();
+        Temperature temperature = new Temperature();
+        Volume volume = new Volume();
+
+        while (isTrue) {
+
+            System.out.println("\n****** Converter Menu ******");
+            System.out.println("1. Length");
+            System.out.println("2. Temperature");
+            System.out.println("3. Weight");
+            System.out.println("4. Volume");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("\nLength Conversions:");
+                    System.out.println("1. Meter to Kilometer");
+                    System.out.println("2. Kilometer to Meter");
+                    System.out.println("3. Meter to Centimeter");
+                    System.out.println("4. Centimeter to Meter");
+
+                    System.out.print("Enter you choice what opertions to perform: ");
+                    int lChoice = sc.nextInt();
+
+                    if (lChoice >= 1 && lChoice <= 4) {
+                        System.out.print("Enter value: ");
+                        double value = sc.nextDouble();
+
+                        switch (lChoice) {
+                            case 1:
+                                System.out.println("Result: " + length.meterToKilometer(value));
+                                break;
+                            case 2:
+                                System.out.println("Result: " + length.kilometerToMeter(value));
+                                break;
+                            case 3:
+                                System.out.println("Result: " + length.meterToCentimeter(value));
+                                break;
+                            case 4:
+                                System.out.println("Result: " + length.centimeterToMeter(value));
+                                break;
+                        }
+                    } else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nTemperature Conversions:");
+                    System.out.println("1. Celsius to Fahrenheit");
+                    System.out.println("2. Fahrenheit to Celsius");
+                    System.out.println("3. Celsius to Kelvin");
+                    System.out.println("4. Kelvin to Celsius");
+
+                    System.out.print("Enter you choice what opertions to perform: ");
+                    int tChoice = sc.nextInt();
+
+                    if (tChoice >= 1 && tChoice <= 4) {
+                        System.out.print("Enter value: ");
+                        double value = sc.nextDouble();
+
+                        switch (tChoice) {
+                            case 1:
+                                System.out.println("Result: " + temperature.celsiusToFahrenheit(value));
+                                break;
+                            case 2:
+                                System.out.println("Result: " + temperature.fahrenheitToCelsius(value));
+                                break;
+                            case 3:
+                                System.out.println("Result: " + temperature.celsiusToKelvin(value));
+                                break;
+                            case 4:
+                                System.out.println("Result: " + temperature.kelvinToCelsius(value));
+                                break;
+                        }
+                    } else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("\nWeight Conversions:");
+                    System.out.println("1. Kilogram to Gram");
+                    System.out.println("2. Gram to Kilogram");
+                    System.out.println("3. Kilogram to Pound");
+                    System.out.println("4. Pound to Kilogram");
+
+
+                    System.out.print("Enter you choice what opertions to perform: ");
+                    int wChoice = sc.nextInt();
+
+                    if (wChoice >= 1 && wChoice <= 4) {
+                        System.out.print("Enter value: ");
+                        double value = sc.nextDouble();
+
+                        switch (wChoice) {
+                            case 1:
+                                System.out.println("Result: " + weight.kgToGram(value));
+                                break;
+                            case 2:
+                                System.out.println("Result: " + weight.gramToKg(value));
+                                break;
+                            case 3:
+                                System.out.println("Result: " + weight.kgToPound(value));
+                                break;
+                            case 4:
+                                System.out.println("Result: " + weight.poundToKg(value));
+                                break;
+                        }
+                    } 
+                    else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("\nVolume Conversion:");
+                    System.out.println("1. Liter to Milliliter:");
+                    System.out.println("2. milliliter To Liter:");
+                    System.out.println("3. liter To CubicMeter:");
+                    System.out.println("4. cubicMeter To Liter:");
+
+
+                    System.out.print("Enter you choice what opertions to perform: ");
+                    int vChoice = sc.nextInt();
+
+                    if (vChoice >= 1 && vChoice <= 4) {
+                        System.out.print("Enter value: ");
+                        double value = sc.nextDouble();
+
+                        switch (vChoice) {
+                            case 1:
+                                System.out.println("Result: " + volume.literToMilliliter(value));
+                                break;
+                            case 2:
+                                System.out.println("Result: " + volume.milliliterToLiter(value));
+                                break;
+                            case 3:
+                                System.out.println("Result: " + volume.literToCubicMeter(value));
+                                break;
+                            case 4:
+                                System.out.println("Result: " + volume.cubicMeterToLiter(value));
+                                break;
+                        }
+                    } 
+                    else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+                case 5:
+                    isTrue = false;
+                    break;            
+                 
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+
+        System.out.println("Thank you!!!!");
+        sc.close();
+    }
+}
---
+++ Volume.java
@@ -0,0 +1,19 @@
+public class Volume {
+
+    public double literToMilliliter(double liter){
+        return liter * 1000;
+    }
+
+    public double milliliterToLiter(double ml){
+        return ml / 1000;
+    }
+
+    public double literToCubicMeter(double liter){
+        return liter /  1000;
+    }
+
+    public double cubicMeterToLiter(double cubicMeter){
+        return cubicMeter * 1000;
+    }
+    
+}
